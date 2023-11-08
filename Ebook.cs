5a359be2882f9ffa5258529766c692c8bb9fde77
Malin Lövqvist <malin.lovqvist@gmail.com>
2023-11-08 15:06:32 +0100

    Ebook class

---
+++ Ebook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtuellt_Boksystem
+{
+    public class Ebook : Book
+    {
+        public int FileSize { get; set; }
+        public string DownloadUrl { get; set; }
+
+        public Ebook(string title, string author, string isbn, int filesize, string downloadurl) 
+        {
+            FileSize = filesize;
+            DownloadUrl = downloadurl;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"{Title} {Author} {ISBN} {FileSize} {DownloadUrl}");
+            base.PrintInfo();
+        }
+
+    }
+}
